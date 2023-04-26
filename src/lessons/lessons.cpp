2955cb721d45b49082dceecc7fbb85745b0fa122
Conrad <122440196+cndrw@users.noreply.github.com>
2023-04-26 07:33:04 +0000

    26.04

--- src/lessons/lessons.cpp
+++ src/lessons/lessons.cpp
@@ -1,24 +1,42 @@
 #include <cstdint>
 #include <iostream>
 
-namespace test
+class Student
 {
-using MyCppStruct = struct
-{
-    int8_t a = 0;
-    float b = 0.0f;
+   private:
+    int16_t bank_account_num;
 
-    auto print() const -> void
+   public:
+    int16_t num_of_exams = 0;
+    int32_t id_number;
+    std::string name;
+
+    Student(std::string par_name, int16_t par_bank_num, int32_t par_id)
+        : bank_account_num{par_bank_num}, id_number{par_id}, name{par_name}
     {
-        std::cout << "a = " << a << "; b = " << b << std::endl;
+    }
+
+    void update_bank_account(const int16_t par_bank_num)
+    {
+        bank_account_num = par_bank_num;
+    }
+
+    void print() const
+    {
+        std::cout << "--------------------------------" << std::endl;
+        std::cout << "Student name: " << name << std::endl;
+        std::cout << "Number of Exams: " << num_of_exams << std::endl;
+        std::cout << "ID-Number: " << id_number << std::endl;
+        std::cout << "Bank Account Number: " << bank_account_num << std::endl;
     }
 };
-}  // namespace test
 
 auto main() -> int
 {
-    test::MyCppStruct my_struct;
-    std::cout << "Printing struct my_struct: " << std::endl;
-    my_struct.print();
+    Student student("Link", 2, 3);
+    student.print();
+    student.update_bank_account(1234);
+    student.print();
+
     return 0;
 }
