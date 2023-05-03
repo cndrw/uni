7c18a1b3f310905172f873ecfa8663d69e5f417d
Conrad <122440196+cndrw@users.noreply.github.com>
2023-05-03 07:28:18 +0000

    03.05

--- src/lessons/lessons.cpp
+++ src/lessons/lessons.cpp
@@ -1,42 +1,9 @@
 #include <cstdint>
 #include <iostream>
 
-class Student
-{
-   private:
-    int16_t bank_account_num;
 
-   public:
-    int16_t num_of_exams = 0;
-    int32_t id_number;
-    std::string name;
-
-    Student(std::string par_name, int16_t par_bank_num, int32_t par_id)
-        : bank_account_num{par_bank_num}, id_number{par_id}, name{par_name}
-    {
-    }
-
-    void update_bank_account(const int16_t par_bank_num)
-    {
-        bank_account_num = par_bank_num;
-    }
-
-    void print() const
-    {
-        std::cout << "--------------------------------" << std::endl;
-        std::cout << "Student name: " << name << std::endl;
-        std::cout << "Number of Exams: " << num_of_exams << std::endl;
-        std::cout << "ID-Number: " << id_number << std::endl;
-        std::cout << "Bank Account Number: " << bank_account_num << std::endl;
-    }
-};
 
 auto main() -> int
 {
-    Student student("Link", 2, 3);
-    student.print();
-    student.update_bank_account(1234);
-    student.print();
-
     return 0;
 }
--- src/test1/main.c
+++ src/test1/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    for(int i = 0; i < 10; i++)
+    int num_of_elements = 0;
+
+    printf("Give size:");
+    scanf("%d", &num_of_elements);
+
+    int* arr = (int*)malloc(num_of_elements * sizeof(int));
+    
+    if(arr == NULL)
     {
-        printf("%d\n", i);
+        printf("Memory not Allocated\n");
+        exit(0);
     }
+
+    for(int i = 0; i < num_of_elements; i++)
+    {
+        arr[i] = i + 1;
+        printf("%d\n", arr[i]); 
+    }
+
+    free(arr);
     
     return 0;
 }
