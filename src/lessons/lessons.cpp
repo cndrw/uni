342439e343e1ebc8d0064bb65732bcc4bd8f7fdb
Conrad <122440196+cndrw@users.noreply.github.com>
2023-05-05 21:18:47 +0000

    05.05

--- src/lessons/lessons.cpp
+++ src/lessons/lessons.cpp
@@ -1,9 +1,23 @@
 #include <cstdint>
 #include <iostream>
+#include <memory>
 
 
 
 auto main() -> int
 {
+    uint size = 0;
+    std::cout << "Size: ";
+    std::cin >> size;
+
+
+    auto u_ptr = std::make_shared<int[]>(size);
+
+    for(uint i = 0; i < size; i++)
+    {
+        u_ptr.get()[i] = i + 1;
+        std::cout << "Element " << i << " ist " << u_ptr.get()[i] << std::endl;  
+    }
+
     return 0;
 }
