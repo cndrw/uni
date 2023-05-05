4721adb98402efa2a1edfb553e67f63c493772cb
Conrad <122440196+cndrw@users.noreply.github.com>
2023-05-05 21:46:06 +0000

    try with shared v3

--- src/lessons/lessons.cpp
+++ src/lessons/lessons.cpp
@@ -5,18 +5,17 @@
 
 auto main() -> int
 {
-    uint size = 0;
+    uint size = 5;
     std::cout << "Size: ";
-    std::cin >> size;
 
 
     auto u_ptr = std::make_shared<int[]>(size);
 
-    // for(uint i = 0; i < size; i++)
-    // {
-    //     u_ptr.get()[i] = i + 1;
-    //     std::cout << "Element " << i << " ist " << u_ptr.get()[i] << std::endl;  
-    // }
+    for(uint i = 0; i < size; i++)
+    {
+        u_ptr.get()[i] = i + 1;
+        std::cout << "Element " << i << " ist " << u_ptr.get()[i] << std::endl;  
+    }
 
     return 0;
 }
