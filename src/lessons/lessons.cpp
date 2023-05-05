95cd5020f33c339b798b781df37df8e3fefe918a
Conrad <122440196+cndrw@users.noreply.github.com>
2023-05-05 23:23:51 +0200

    test changes

--- src/lessons/lessons.cpp
+++ src/lessons/lessons.cpp
@@ -11,12 +11,12 @@ auto main() -> int
     std::cin >> size;
 
 
-    auto u_ptr = std::make_shared<int[]>(size);
+    auto u_ptr = new int[size];
 
     for(uint i = 0; i < size; i++)
     {
-        u_ptr.get()[i] = i + 1;
-        std::cout << "Element " << i << " ist " << u_ptr.get()[i] << std::endl;  
+        u_ptr[i] = i + 1;
+        std::cout << "Element " << i << " ist " << u_ptr[i] << std::endl;  
     }
 
     return 0;
