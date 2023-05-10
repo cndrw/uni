94aabdd343a73adc8d0218117eaa5eee00d1f344
Conrad <122440196+cndrw@users.noreply.github.com>
2023-05-10 07:36:21 +0000

    today

--- src/lessons/lessons.cpp
+++ src/lessons/lessons.cpp
@@ -3,12 +3,51 @@
 #include <memory>
 
 
+class Shape
+{
+    public: 
+    void set_width(int8_t width_in)
+    {
+        m_width_ = width_in;
+    }
+
+    void set_height(int8_t height_in)
+    {
+        m_height_ = height_in;
+    }
+
+    protected:
+    int8_t m_width_;
+    int8_t m_height_;
+};
+
+class PaintCost
+{
+    public:
+    static auto get_cost(double area) -> double
+    {
+        return area * 70;
+    }
+};
+
+class Triangle : public Shape, public PaintCost
+{
+    public:
+    auto get_area() -> double
+    {
+        return 0.5f * (m_width_ * m_height_);
+    }
+};
+
 auto main() -> int
 {
-    uint size = 5;
-    std::cout << "Size: ";
+    Triangle triangle;
+    triangle.set_height(5);
+    triangle.set_width(5);
 
+    double area = triangle.get_area();
 
+<<<<<<< HEAD
     // auto u_ptr = std::make_shared<int[]>(size);
 
     std::shared_ptr<int[]> u_ptr(new int[size]);
@@ -23,6 +62,10 @@ auto main() -> int
         u_ptr.get()[i] = i + 1;
         std::cout << "Element " << i << " ist " << u_ptr.get()[i] << std::endl;  
     }
+=======
+    std::cout << "Fläche ist: " << area << std::endl;
+    std::cout << "Kosten sind: " << PaintCost::get_cost(area) << std::endl;
+>>>>>>> 34ff4ec (10.05)
 
     return 0;
 }
