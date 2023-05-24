b567991666c278732336908373ec376b804fa54d
Conrad <122440196+cndrw@users.noreply.github.com>
2023-05-24 14:55:28 +0000

    branch test

--- src/H2-2/src/spider.cpp
+++ src/H2-2/src/spider.cpp
@@ -13,10 +13,13 @@ Spider::~Spider()
     Animal::die("Spider");
 }
 
+// Just some useless comment
+
 void Spider::move(float move_time) 
 {
     std::cout << "i don't know which sound a spide does" << std::endl;
     xpos_ += speed_ * move_time;
     ypos_ += speed_ * move_time; 
     std::cout << "Spider moved to X: " << xpos_ << " Y: " << ypos_ << std::endl;
+    std::cout << "just a filler text" << std::endl;
 }
